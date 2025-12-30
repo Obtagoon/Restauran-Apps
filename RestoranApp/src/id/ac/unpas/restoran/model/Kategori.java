0ee28f9bb0cea997f42bb56d88d3daadf0d8a7a5
haekal3 <haekalkhadafi21@gmail.com>
2025-12-30 17:52:25 +0700

    refactor: Package database, model, dan controller

--- RestoranApp/src/id/ac/unpas/restoran/model/Kategori.java
+++
@@ -1,6 +0,0 @@
-package id.ac.unpas.restoran.model;
-
-public class Kategori {
-    // TODO: Silakan isi atribut, constructor, getter & setter di sini
-    // Sesuaikan atribut dengan kolom di database!
-}
---
+++ RestoranApp/src/main/java/id/ac/unpas/restoranapp/RestoranApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.restoranapp;
+
+/**
+ *
+ * @author KATANA 15
+ */
+public class RestoranApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
